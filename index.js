eb08e3d225ef2c3830db12a4ce66d8e03380c161
jschnapper <jschnap717@gmail.com>
2019-01-08 23:18:39 -0800

    passportjs added and google oauth setup with keys

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const passport = require('passport');
+// The strategy of choice we are using for passport
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 // Creates express application
 const app = express();
@@ -8,10 +12,51 @@ An express route handler that is watching for specific http requests with a spec
 req: a js object representing the incoming request
 res: data to be sent back to whoever made the request
 */
-app.get('/', (req, res) => {
-    res.send({hi: 'potato'});
-});
 
+
+
+
+// No longer relevant since we are using passport
+
+// app.get('/', (req, res) => {
+//     res.send({hi: 'potato'});
+// });
+
+// Declares a new instance of the google passport stragegy 
+// Enables the application to authenticate with google
+// Within GoogleStrategy we provide a configuration to inform how to autheticate within the application
+// passport.use is a generic register to indicate a specific strategy
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'            
+        }, 
+        accessToken => {
+            console.log(accessToken);
+        }
+    )
+);
+
+// Route handler to launch authentication with passport using the strategy called google 
+// GoogleStrategy contains its own identifier which is the string 'google'
+app.get(
+    '/auth/google',
+    passport.authenticate('google', 
+        {
+            scope: ['profile', 'email']
+        }
+    )
+);
+
+// This is the callback function the google will send the code to
+// Using the information in the response from google, passport will be able to get the `code` that we need to get profile information
+// It looks the same as above, but the response uri contains the code and passport knows how to handle it
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google')
+);
 
 /* 
 Dynamically assign the port to listen to.
