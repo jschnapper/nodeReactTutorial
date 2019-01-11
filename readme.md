0fcd1b49fefd223388f82186640c44eac761d6b2
jschnapper <jschnap717@gmail.com>
2019-01-10 16:51:23 -0800

    handled promise in user creation. Checks for user id when signing in

---
+++ readme.md
@@ -0,0 +1,1 @@
+A sample application made with node and react, following a tutorial made by Stephen Grider
--- services/passport.js
+++ services/passport.js
@@ -17,17 +17,36 @@ const User = mongoose.model('users');
 // Within GoogleStrategy we provide a configuration to inform how to autheticate within the application
 // passport.use is a generic register to indicate a specific strategy
 passport.use(
-    new GoogleStrategy(
-        {
+    new GoogleStrategy({
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'            
-        }, 
+            callbackURL: '/auth/google/callback'
+        },
         (accessToken, refreshToken, profile, done) => {
-            // use '.save()' function to add to database
-            new User({
-                googleId: profile.id
-            }).save();
+            // Check to see if new user, an asyc action
+            // Anytime connecting with db, it is async
+            // The query returns a promise
+            User.findOne({
+                    googleId: profile.id
+                })
+                .then(existingUser => {
+                    if (existingUser) {
+                        // There is already record for this user
+                        // Use passport's done function to indicate the process is over
+                        done(null, existingUser);
+                    } else {
+                        // There is no record for this user, create a new user
+                        // use '.save()' function to add to database
+                        // Asyc; call done once saved
+                        new User({
+                                googleId: profile.id
+                            })
+                            .save()
+                            .then(user => done(null, user));
+                    }
+                })
+
+
         }
     )
 );
